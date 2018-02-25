cd6e1a92d98209e250a8201bcb395890474dd714
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-02-25 20:00:43 +0100

    add getDataCustomerDiscountHistory

--- src/Syscover/ShoppingCart/CartPriceRules.php
+++ src/Syscover/ShoppingCart/CartPriceRules.php
@@ -9,23 +9,28 @@ class CartPriceRules extends Collection
      *
      * @param int $customerId
      * @param int $orderId
+     * @param bool $applied
      * @return array
      */
-    public function getDataCustomerDiscountHistory(int $customerId, int $orderId)
+    public function getDataCustomerDiscountHistory(int $customerId, int $orderId, bool $applied)
     {
         $data = [];
         foreach ($this as $discount)
         {
+            $priceRule = $discount->options->priceRule;
+
             $dataAux = [];
             $dataAux['customer_id']                             = $customerId;
             $dataAux['order_id']                                = $orderId;
+            $dataAux['applied']                                 = $applied;
+            $dataAux['discount_amount']                         = $discount->discountAmount;
+            $dataAux['rule_type']                               = get_class($priceRule);
+            $dataAux['rule_id']                                 = $priceRule->id;
+            $dataAux['price_rule']                              = $priceRule;
 
-            //$dataAux['has_coupon']                              = $discount->;
-
-
-
-            $data[] = $dataAux;
+            $data[] = array_merge($dataAux, $priceRule->toArray());
         }
+
         return $data;
     }
 }
