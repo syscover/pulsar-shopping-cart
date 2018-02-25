a92d8561bd4a8df6b9924f40cd8c411ee593410f
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-02-25 18:11:28 +0100

    getDataCustomerDiscountHistory

--- src/Syscover/ShoppingCart/CartPriceRules.php
+++ src/Syscover/ShoppingCart/CartPriceRules.php
@@ -4,5 +4,28 @@ use Illuminate\Support\Collection;
 
 class CartPriceRules extends Collection
 {
+    /**
+     * Create array object with values to create a order
+     *
+     * @param int $customerId
+     * @param int $orderId
+     * @return array
+     */
+    public function getDataCustomerDiscountHistory(int $customerId, int $orderId)
+    {
+        $data = [];
+        foreach ($this as $discount)
+        {
+            $dataAux = [];
+            $dataAux['customer_id']                             = $customerId;
+            $dataAux['order_id']                                = $orderId;
 
+            //$dataAux['has_coupon']                              = $discount->;
+
+
+
+            $data[] = $dataAux;
+        }
+        return $data;
+    }
 }
