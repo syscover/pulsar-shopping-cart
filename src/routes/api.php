87f80978e275313ddaf4a67deae37032197636f2
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-13 13:39:08 +0200

    minor fix

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => ['sessions']], function () {
     | PRODUCTS
     |----------------------------------
     */
-    Route::get('api/v1/shopping-cart/item/{instance}',          'Syscover\ShoppingCart\Controllers\ShoppingCartController@index')->name('api.shopping_cart.item');
+    Route::get('api/v1/shopping-cart/item/{instance?}',          'Syscover\ShoppingCart\Controllers\ShoppingCartController@index')->name('api.shopping_cart.item');
 //    Route::get('api/v1/market/product/{id}/{lang}',                             'Syscover\Market\Controllers\ProductController@show')->name('api.market.show_product');
 //    Route::post('api/v1/market/product/search',                                 'Syscover\Market\Controllers\ProductController@search')->name('api.market.search_product');
 //    Route::post('api/v1/market/product',                                        'Syscover\Market\Controllers\ProductController@store')->name('api.market.store_product');
