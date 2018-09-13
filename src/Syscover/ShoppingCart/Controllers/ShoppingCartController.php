70c56214898d0ac38eb7d7c8f83aea9bc170afb3
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-13 17:30:42 +0200

    minor fix

--- src/Syscover/ShoppingCart/Controllers/ShoppingCartController.php
+++ src/Syscover/ShoppingCart/Controllers/ShoppingCartController.php
@@ -14,6 +14,15 @@ class ShoppingCartController extends BaseController
     {
         $response['status']     = 200;
         $response['statusText'] = "OK";
+        $response['data']       = CartProvider::instance(request('instance') ?? null);
+
+        return response()->json($response);
+    }
+
+    public function items()
+    {
+        $response['status']     = 200;
+        $response['statusText'] = "OK";
         $response['data']       = CartProvider::instance(request('instance') ?? null)->getCartItems();
 
         return response()->json($response);
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
 Route::group(['middleware' => ['sessions']], function () {
+
     /*
     |----------------------------------
-    | PRODUCTS
+    | SHOPPING CART
     |----------------------------------
     */
-    Route::get('api/v1/shopping-cart/item/{instance?}',          'Syscover\ShoppingCart\Controllers\ShoppingCartController@index')->name('api.shopping_cart.item');
+    Route::get('api/v1/shopping-cart/{instance?}',              'Syscover\ShoppingCart\Controllers\ShoppingCartController@index')->name('api.shopping_cart.item');
+
+    /*
+    |----------------------------------
+    | ITEMS
+    |----------------------------------
+    */
+    Route::get('api/v1/shopping-cart/item/{instance?}',          'Syscover\ShoppingCart\Controllers\ShoppingCartController@items')->name('api.shopping_cart.item');
 //    Route::get('api/v1/market/product/{id}/{lang}',                             'Syscover\Market\Controllers\ProductController@show')->name('api.market.show_product');
 //    Route::post('api/v1/market/product/search',                                 'Syscover\Market\Controllers\ProductController@search')->name('api.market.search_product');
 //    Route::post('api/v1/market/product',                                        'Syscover\Market\Controllers\ProductController@store')->name('api.market.store_product');
