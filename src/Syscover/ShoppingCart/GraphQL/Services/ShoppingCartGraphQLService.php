ff9854e0fee9b355d534526f82a6cb0fbc993991
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-26 14:06:03 +0200

    change graphql

--- src/Syscover/ShoppingCart/GraphQL/Services/ShoppingCartGraphQLService.php
+++ src/Syscover/ShoppingCart/GraphQL/Services/ShoppingCartGraphQLService.php
@@ -6,11 +6,11 @@ class ShoppingCartGraphQLService
 {
     public function cart($root, array $args)
     {
-        return CartProvider::instance($args['instance'] ?? null)->toArray();
+        return CartProvider::instance($args['guard'] ?? null)->toArray();
     }
 
     public function items($root, array $args)
     {
-        return CartProvider::instance($args['instance'] ?? null)->getCartItems()->toArray();
+        return CartProvider::instance($args['guard'] ?? null)->getCartItems()->toArray();
     }
 }
